94c047eb94767e3ed9a2e42a3e670c65b0520672
Addis <xdaddisxd@gmail.com>
2025-08-11 16:26:02 -0400

    multiple changes in 90 files

--- src/lib/ui/Select/index.ts
+++
@@ -1,5 +0,0 @@
-import Select from './Select';
-
-export default Select;
-export { default as useSelect } from './hooks/useSelect';
-export type { Option, SelectProps } from './Select.d';
--- src/lib/ui/Tab/index.ts
+++
@@ -1,6 +0,0 @@
-import Tabs, { TabButton } from './Tab';
-
-export { useTabs } from './useTab';
-export { TabButton };
-export type { TabsProps, TabButtonProps, TabItem } from './Tab.d';
-export default Tabs;
